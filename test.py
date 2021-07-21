513b71f14d47ce902897d6edc09c4da8c35a6ea2
Vėjas Pinkevičius <vejaspink@gmail.com>
2021-07-21 16:04:39 +0300

    Added build exe

--- test.py
+++
@@ -1,5 +0,0 @@
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-
-driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
-driver.get('https://www.15min.lt/')
